f0596d8121e2c4faa261e096192bccc4fed9cb9b
Juwairyh <juwairyhh12@gmail.com>
2024-09-21 14:00:05 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * HelloWorld
+ * Nama: Juwairiyah
+ * NIM: 2411016320011
+ */
+public class HelloWorld {
+public static void main(String[] args) {
+    // cltl + shift +F : auto format
+    // string menggunakan tanda petik 2 (" ")
+    System.out.println("Hello World");
+    System.out.println("Nama: Juwairiyah");
+    System.out.println("Nim: 2411016320011");
+
+    // angka (integer/double/float) tidak perlu tanda petik
+    System.out.println(13);
+    System.out.println(true);
+    
+    // char menggunakan tanda petik 1 (' ')
+    System.out.println('A');
+}
+}
